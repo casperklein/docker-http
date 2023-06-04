30ef37dedb7c5ecda06c600674521ad0adbf9987
casperklein <casperklein@users.noreply.github.com>
2023-06-04 03:16:12 +0200

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 set -ueo pipefail
 
+DIR=${0%/*}
+cd "$DIR"
+
 VERSION=$(jq -er '.version'		< config.json)
 IMAGE=$(jq -er '.image'			< config.json)
 TAG=$(jq -er '"\(.image):\(.version)"'	< config.json)
 
-DIR=${0%/*}
-cd "$DIR"
-
 echo "Building: $TAG"
 echo
-docker build -t "$TAG" --build-arg VERSION="$VERSION" .
+docker build -t "$TAG" --build-arg VERSION="$VERSION" --provenance=false .
 docker tag "$TAG" "$IMAGE:latest"
